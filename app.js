fc0c1027f85b46a94ff9ea1535ba563ae39cb46f
jameswatt <43647262@qq.com>
2021-08-31 12:13:21 +0800

    首页路由修改

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
+// 新增路由
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+var apiRouter = require('./routes/api');
+var plRouter = require('./routes/pl');
+var sdRouter = require('./routes/sd');
+var socketio = require('./socket.io/index');
 
 const app = express();
 
@@ -9,7 +17,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 
-app.use('/', routes);
+// app.use('/', routes);
+app.use('/', indexRouter);
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
