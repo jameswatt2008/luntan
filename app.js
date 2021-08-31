906895a26aeb62726462d5d71b6e53b9a6dcdb85
jameswatt <43647262@qq.com>
2021-08-31 16:12:38 +0800

    增加模板引擎

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.all('*', function (req, res, next) {
 });
 
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
