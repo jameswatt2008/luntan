a63849c20a0fcdbafdac386501d32bece13a8cf3
jameswatt <43647262@qq.com>
2021-08-31 13:19:14 +0800

    path 注释

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 
@@ -27,8 +27,8 @@ app.all('*', function (req, res, next) {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // app.use(logger('dev'));
 // app.use(express.json());
