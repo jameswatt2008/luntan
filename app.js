ffaed3b8a1d2dc10af553301e06061b2039ecda1
jameswatt <43647262@qq.com>
2021-08-31 13:30:54 +0800

    appjs-001

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes/index');
+var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+var apiRouter = require('./routes/api');
+var plRouter = require('./routes/pl');
+var sdRouter = require('./routes/sd');
+// var socketio = require('./socket.io/index');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+
 
 
 const app = express();
+//跨域
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.type('html');
+    next();
+});
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 
 // default catch all handler
