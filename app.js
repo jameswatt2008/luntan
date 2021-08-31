b94f5bc0c96d4a5065e739e2c9ad012b57cef70b
jameswatt <43647262@qq.com>
2021-08-31 13:42:59 +0800

    注释请求头

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ const routes = require('./routes/index');
 // var sdRouter = require('./routes/sd');
 // var socketio = require('./socket.io/index');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 
 
 
 const app = express();
 //跨域
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    res.type('html');
-    next();
-});
+// app.all('*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By", ' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     res.type('html');
+//     next();
+// });
 
 
 // app.set('views', path.join(__dirname, 'views'));
