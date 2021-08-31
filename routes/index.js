185fa568887e33507805b91e46c30c530a7bc050
jameswatt <43647262@qq.com>
2021-08-31 13:47:47 +0800

    模板测试

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ r.get('/', (req, res) => res.json(new SuccessResponseObject('接口测试')));
 
 /* GET home page. */
 r.get('/test', function (req, res, next) {
-    // res.render('index', { title: '页面测试' });
-    res.send("hello test");
+    res.render('index', { title: '页面测试' });
+    // res.send("hello test");
 });
 
 module.exports = r;
