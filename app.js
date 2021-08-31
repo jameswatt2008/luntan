cf3b4ba07d3cca4c483a5bf5838f6017676af7b9
jameswatt <43647262@qq.com>
2021-08-31 13:35:04 +0800

    cookie和静态注释

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
