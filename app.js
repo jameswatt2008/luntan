b50c0db68753c960f944db29fa97b3f8aa985dbf
jameswatt <43647262@qq.com>
2021-08-31 16:21:20 +0800

    增加其他路由

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var postRouter = require('./routes/post');
-// var apiRouter = require('./routes/api');
-// var plRouter = require('./routes/pl');
-// var sdRouter = require('./routes/sd');
+var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+var apiRouter = require('./routes/api');
+var plRouter = require('./routes/pl');
+var sdRouter = require('./routes/sd');
 // var socketio = require('./socket.io/index');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
@@ -22,7 +22,7 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
-    res.type('html');//这个方法有问题，服务器上报错
+    res.type('html');//
 
     next();
 });
